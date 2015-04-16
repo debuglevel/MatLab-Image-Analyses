00f88f5c29125c161821e1c52bc05d64db3ad84f
Marc V. Kohaupt <debuglevel@gmail.com>
2015-04-16 20:49:49 +0200

    fixed error on missing year

--- getImageMetadata.m
+++ getImageMetadata.m
@@ -9,16 +9,25 @@ function [filename, city, citybrewery, state, year, subyear, extension] = getIma
 % last change and history: see version control system
 % created with Octave 3.8.1 i686-pc-linux-gnu
 
-matches = regexp(path, './pics/([a-z_-]*)_([0-9]*)_([a-z-]*)_([0-9]*)([a-z]*)\.([a-z]*)', 'tokens');
+matches = regexp(path, './pics/([a-z_-]*)_([0-9]*)_([a-z-]*)_?([0-9]*)([a-z]*)\.([a-z]*)', 'tokens');
+
+groups = size(matches{1});
+groups = groups(2);
 
 city = matches{1}{1};
 citybrewery = matches{1}{2};
 state = matches{1}{3};
-year = matches{1}{4};
-subyear = matches{1}{5};
-extension = matches{1}{6};
+if (groups == 6)
+  year = matches{1}{4};
+  subyear = matches{1}{5};
+  extension = matches{1}{6};
+else
+  year = '';
+  subyear = '';
+  extension = matches{1}{5};
+end
 
-temp = strread(path,'%s','delimiter','/')
+temp = strread(path,'%s','delimiter','/');
 filename = temp{end};
 
 end
